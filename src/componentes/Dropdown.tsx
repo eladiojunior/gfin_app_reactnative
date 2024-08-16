3c0a04d6ed136f452218cd45d8ab8342813278e7
Eladio Júnior <eladiojunior@gmail.com>
2024-08-15 23:21:06 -0300

    Dropbox implementado, ufa!

--- src/componentes/Dropdown.tsx
+++ src/componentes/Dropdown.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { FlatList, I18nManager, Image, Modal, Pressable, SafeAreaView, ScrollView, StyleSheet, Text, TouchableHighlight, View } from "react-native";
+import { Alert, FlatList, I18nManager, Image, Modal, Pressable, SafeAreaView, ScrollView, StyleSheet, Text, TouchableHighlight, View } from "react-native";
 import Colors from "../constants/Colors";
 import { TouchableOpacity, TouchableWithoutFeedback } from "react-native-gesture-handler";
 import Button from "./Button";
@@ -10,10 +10,7 @@ const Dropdown = (props: any) => {
   const {
     items = [
       { label: 'Item 01', value: '01' }, { label: 'Item 02', value: '02' }, { label: 'Item 03', value: '03' }, { label: 'Item 04', value: '04' }, { label: 'Item 05', value: '05' }, 
-      { label: 'Item 06', value: '06' }, { label: 'Item 07', value: '07' }, { label: 'Item 08', value: '08' }, { label: 'Item 09', value: '09' }, { label: 'Item 10', value: '10' }, 
-      { label: 'Item 11', value: '11' }, { label: 'Item 12', value: '12' }, { label: 'Item 13', value: '13' }, { label: 'Item 14', value: '14' }, { label: 'Item 15', value: '15' }, 
-      { label: 'Item 16', value: '16' }, { label: 'Item 17', value: '17' }, { label: 'Item 18', value: '18' }, { label: 'Item 19', value: '19' }, { label: 'Item 20', value: '20' }, 
-      { label: 'Item 21', value: '21' }, { label: 'Item 22', value: '22' }, { label: 'Item 23', value: '23' }, { label: 'Item 24', value: '24' }, { label: 'Item 25', value: '25' }],
+      { label: 'Item 06', value: '06' }, { label: 'Item 07', value: '07' }, { label: 'Item 08', value: '08' }, { label: 'Item 09', value: '09' }, { label: 'Item 10', value: '10' }],
     selectedItem = null,
     placeholder = 'Selecione...',
     width = 200,
@@ -40,31 +37,24 @@ const Dropdown = (props: any) => {
   const _renderListItens = () => {
     if (!isModalVisible) return;
     return (
-      <View>
-        <ScrollView>
-          {items.map((item: any) => { return (_renderItem(item)); })}
-        </ScrollView>
-      </View>
+      <ScrollView>
+        {items.map((item: any) => { return (_renderItem(item)); })}
+      </ScrollView>
     );
   }
 
   const _renderItem = (item: any) => {
     const isSelected = (currentValue === item.value);
     return (
-      <TouchableOpacity key={item.value} style={{ zIndex: 999 }} onPress={() => _selectItem(item)}>
-        <View style={StyleSheet.flatten([isSelected && styles.textItemSelected])}>
-          <View style={styles.item}>
-            <Text style={StyleSheet.flatten([isSelected ? styles.textItemSelected : styles.textItem])}>
-              {item.label}
-            </Text>
-          </View>
+      <Pressable key={item.value} onPress={() => _selectItem(item)}>
+        <View style={StyleSheet.flatten([isSelected && styles.textItemSelected, styles.item])}>
+          <Text style={StyleSheet.flatten([isSelected ? styles.textItemSelected : styles.textItem])}>{item.label}</Text>
         </View>
-      </TouchableOpacity>
+      </Pressable>
     );
   };
 
   const _selectItem = (item: any) => {
-    console.log(item);
     setCurrentValue(item.value);
     onChangeItem(item);
     setModalVisible(false);
@@ -72,6 +62,7 @@ const Dropdown = (props: any) => {
 
   return (
     <View>
+
       <TouchableOpacity onPress={() => setModalVisible(true)}>
         <View style={StyleSheet.flatten([styles.dropdown, { minWidth: width, height: height },])}>
           <Text style={styles.textItem}>
